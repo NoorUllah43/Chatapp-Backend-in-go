3c5497aa5f523f5948e559c3a18dc860481c0a46
NoorUllah43 <noorullah382040.com>
2025-06-17 12:12:13 +0500

    setup project and connect to postgresql database

---
+++ db/connectDB.go
@@ -0,0 +1,27 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	"github.com/gofiber/fiber/v2/log"
+)
+
+var DB *sql.DB
+
+func ConnectPostgresql() {
+
+	connectionString := fmt.Sprintf("host=%v port=%v user=%v password=%v dbname=%v sslmode=disable", os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
+	fmt.Print(connectionString)
+
+	db, err := sql.Open("postgres", connectionString)
+
+	if err != nil {
+		log.Errorf("database not connecting error: ", err)
+	}
+
+	DB = db
+	log.Info("database connected to postgresql")
+
+}
--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
-func main(){
-	
+import (
+	"fmt"
+	"log"
+
+	"github.com/NoorUllah43/Chatapp-Backend-in-go.git/routes"
+	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/cors"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+
+	app := fiber.New()
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Println("env err:", err)
+	}
+
+	app.Use(cors.Config{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		AllowMethods: []string{"GET", "POST", "HEAD", "PUT", "DELETE", "PATCH", "OPTIONS"},
+	})
+
+
+	routes.AuthRoutes(app)
+
+
+	log.Fatal(app.Listen(":3000"))
+
 }
